541dd1c48cf5d89961b661924f052dde83a01260
tunaslim <tunaslim@gmail.com>
2025-06-15 16:57:11 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+    </h1>
     <div className={styles.page}>
       <main className={styles.main}>
         <Image
